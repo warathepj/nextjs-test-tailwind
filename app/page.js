836078294f38d5cd82b6b944bdda59c2d83af841
warathepj <80021220+warathepj@users.noreply.github.com>
2024-06-02 09:36:47 +0700

    mod .gitignore next.config.mjs

--- app/page.js
+++ app/page.js
@@ -4,6 +4,14 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+
+        <div className="bg-red-100 text-black p-6 w-96 h-48 mr-4 block">
+          <p className="break-words text-2xl">card 1 aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
+        </div>
+        <div className="bg-red-100 text-black p-6 w-96 h-48 block">
+          <p className="break-words">card 2 bbbbbbbnnnnnnnnnnnnnnn11111111111111111111112222222222222222222223333333333333333333333444444444555555nnnnnnnnpppppppppppppppppppppppppppppppppppppppppppppppppppppbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
+        </div>
+
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
           <code className="font-mono font-bold">app/page.js</code>
