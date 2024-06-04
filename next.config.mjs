2cde0887973184e3e73c667b8e697eb0684886ab
warathepj <80021220+warathepj@users.noreply.github.com>
2024-06-04 14:25:39 +0700

    mod next js workflow

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    distDir: 'dist'
+    distDir: 'dist',
+    images: {
+        unoptimized: true,
+    }
 };
 
 export default nextConfig;
